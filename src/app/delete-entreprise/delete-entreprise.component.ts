57fa8d1dea2cd2b8d3bcbfedde7881fc6dce0c9e
Fatou726 <fatoucha726@gmail.com>
2024-10-16 17:18:05 +0000

    commmited
    bouton supprimer

--- src/app/delete-entreprise/delete-entreprise.component.ts
+++ src/app/delete-entreprise/delete-entreprise.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Entreprise } from '../models/entreprise';
+import { EntrepriseService } from '../services/entreprise.service';
+import { FormsModule, NgModel} from '@angular/forms';
+
 
 @Component({
   selector: 'app-delete-entreprise',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './delete-entreprise.component.html',
   styleUrl: './delete-entreprise.component.css'
 })
 export class DeleteEntrepriseComponent {
+  idEntreprise:number=0;
+  entreprise:Entreprise=new Entreprise();
+  constructor(private entrepriseService:EntrepriseService, private route:ActivatedRoute,private router:Router){}
+  ngOnInit():void{
+    this.idEntreprise=this.route.snapshot.params['idEntreprise'];
+    this.entrepriseService.getEntrepriseById(this.idEntreprise).subscribe(data=>{
+      this.entreprise=data;
+    })
+  }
 
+  onSubmit(){
+    this.entrepriseService.deleteEntrepriseById(this.idEntreprise).subscribe(data=>{ 
+      this.goToEntrepriseList();
+    })
+  }
+
+  goToEntrepriseList(){
+    this.router.navigate(['/entreprises'])
+  }
   
 
 }
